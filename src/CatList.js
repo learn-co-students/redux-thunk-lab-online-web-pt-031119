58592a128d84b380e711159921e7c1e8cf1c0e4c
Michael Schiumo <mschiumo18@gmail.com>
2020-01-15 12:41:51 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
 // write your CatList component here
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CatList extends Component {
+
+
+  render() {
+    const { catPics } = this.props;
+
+    <div>
+      {catPics.map(catPic => <img>{catPic}</img>)}
+    </div>
+  }
+
+}
